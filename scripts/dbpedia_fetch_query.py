b1c29190c0e227f7f6f5832fee6e1ac7460f3228
Niels <niels.bobet@tum.de>
2024-11-21 10:14:42 +0100

    ID filter as proposed by Leon

--- scripts/dbpedia_fetch_query.py
+++ scripts/dbpedia_fetch_query.py
@@ -1,6 +1,8 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 
 def keep(string):
+    if "ID" in string:
+        return False
     match string:
         case "wikiPageWikiLink":
             return False
@@ -29,7 +31,7 @@ def keep(string):
 
 
 def get_person_data(person):
-    a=0
+    not_good = 0
     total =0
     good_ones = 0
 
@@ -67,7 +69,7 @@ def get_person_data(person):
         results = sparql.query().convert()
 
         # Process and print results
-        print("Marie Curie - DBpedia Properties:")
+        print(f"{person} - DBpedia Properties:")
         print("-" * 50)
 
         for result in results["results"]["bindings"]:
@@ -77,22 +79,20 @@ def get_person_data(person):
             # Clean up the property name by removing the URI prefix
             prop_name = prop.split("/")[-1]
             if keep(prop_name):
-                if "ID" not in prop_name:
-                    good_ones += 1
-                    if val.startswith('http'):
-                        # Split the URL by '/' and get the last non-empty element
-                        segments = [seg for seg in val.split('/') if seg]
-                        print(f"{prop_name}: {segments[-1]}")
-                        #return segments[-1] if segments else text
-                    else:
-                        print(f"{prop_name}: {val}")
-
+                good_ones += 1
+                if val.startswith('http'):
+                    # Split the URL by '/' and get the last non-empty element
+                    segments = [seg for seg in val.split('/') if seg]
+                    print(f"{prop_name}: {segments[-1]}")
+                    #return segments[-1] if segments else text
+                else:
+                    print(f"{prop_name}: {val}")
 
             else:
-                a+=1
+                not_good+=1
             total+=1
 
-        print("total is:",total," total shitty: ",a," so total good: ",good_ones)
+        print("total is:",total," total shitty: ",not_good," so total good: ",good_ones)
 
     except Exception as e:
         print(f"An error occurred: {e}")
