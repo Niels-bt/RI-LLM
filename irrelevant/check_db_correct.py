c8db664a118d99d87bea16257d6f5bd217d9bbb3
Leon Endres <endresleon01@gmail.com>
2024-12-13 12:53:12 +0100

    wd_master second check and refactoring

--- irrelevant/check_db_correct.py
+++ irrelevant/check_db_correct.py
@@ -1,11 +1,11 @@
 import dbpedia_fetch_query as db_f_q
 import requests as req
 
-file_celeb = open("../topics/celebrities/wd_db_celeb.csv",mode='r')
-file_chm = open("../topics/chemical_elements/wd_db_chemical_elements.csv",mode='r')
-file_constellations = open("../topics/constellations/wd_db_constellations.csv",mode='r')
-file_movies = open("../topics/movies/wd_db_movies.csv",mode='r')
-file_sp500 = open("../topics/sp500/wd_db_sp500.csv",mode='r')
+file_celeb = open("../topics/celebrities/wd_db_celeb.csv", mode='r')
+file_chm = open("../topics/chemical_elements/wd_db_chemical_elements.csv", mode='r')
+file_constellations = open("../topics/constellations/wd_db_constellations.csv", mode='r')
+file_movies = open("../topics/movies/wd_db_movies.csv", mode='r')
+file_sp500 = open("../topics/sp500/wd_db_sp500.csv", mode='r')
 
 files=[file_sp500,file_celeb,file_chm,file_constellations,file_movies]
 
