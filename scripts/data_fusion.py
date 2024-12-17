f2744d3cf80a71441777475fd084ed94011fef9f
Leon Endres <endresleon01@gmail.com>
2024-12-17 16:42:57 +0100

    fix of the fix of the fix

--- scripts/data_fusion.py
+++ scripts/data_fusion.py
@@ -173,9 +173,9 @@ if __name__ == "__main__":
     data_fusion = data_fusion("Coldplay", "Q45188", "celebrities")
     print("--------------------------------------------------")
     for tuple in data_fusion:
-        if isinstance(tuple[1], str): print(tuple[1])
+        if isinstance(tuple[0], str): print(tuple[0])
         else:
-            for label in tuple[1]:
+            for label in tuple[0]:
                 print(label)
         for matched_value in tuple[1]:
             print("--*--" + matched_value)
