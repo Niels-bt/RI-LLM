7596a9cb3d30eb7850bf74d6beb61ebca1d89831
Leon Endres <endresleon01@gmail.com>
2025-01-22 17:37:09 +0100

    first scoring and big refactoring

--- scripts/decision_algo_v3.py
+++ scripts/decision_algo_v3.py
@@ -269,8 +269,8 @@ def process_entities(entities_dir, output_dir):
             resolve_inconsistencies(input_file_path, output_file_path, entity_name)
 
 if __name__ == "__main__":
-    entities_dir = "../topics/chemical_elements/entities"
-    output_dir = "../topics/chemical_elements/llm_correct_entities"
+    entities_dir = "../topics/celebrities/entities"
+    output_dir = "../topics/celebrities/corrected_chatgpt"
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
--- scripts/scoring.py
+++ scripts/scoring.py
@@ -1,62 +1,66 @@
 import re
 
-def scoring():
 
-    domains = ["celebrities", "chemical_elements", "constellations", "movies", "sp500"]
 
-    for domain in domains:
+domains = ["celebrities", "chemical_elements", "constellations", "movies", "sp500"]
 
-        # Opens the file containing all entity ids
-        property_file = open(f"../topics/{domain}/wd_db_{domain}.csv", mode='r', encoding='utf-8')
+for domain in domains:
 
-        line_counter = 0
-        for entity in property_file:
+    # Opens the file containing all entity ids
+    property_file = open(f"../topics/{domain}/wd_db_{domain}.csv", mode='r', encoding='utf-8')
 
-            # Only do the first 10 entities
-            if line_counter >= 10: break
-            line_counter += 1
+    line_counter = 0
+    for entity in property_file:
 
-            # The file_name is computed from the entity file
-            file_name: str = re.split(r'''((?:[^,"']|"[^"]*"|'[^']*')+)''', entity)[1::2][1].removeprefix(" ").replace(" ", "_").replace(":", "").lower()
+        # Only do the first 10 entities
+        if line_counter >= 10: break
+        line_counter += 1
+
+        # The file_name is computed from the entity file
+        file_name: str = re.split(r'''((?:[^,"']|"[^"]*"|'[^']*')+)''', entity)[1::2][1].removeprefix(" ").replace(" ", "_").replace(":", "").lower()
+
+        # We skip tesla and beyoncé because the files are broken
+        if file_name != "tesla" and file_name != "beyoncé":
 
             # Collect the labels and values from the llm
-            llm_values = [()]
-            llm_file = open(f"../topics/{domain}/llm_correct_entities/{file_name}_resolved.csv", mode='r', encoding='utf-8')
+            llm_values = []
+            llm_file = open(f"../topics/{domain}/chatgpt_correct_entities/{file_name}_resolved.csv", mode='r', encoding='utf-8')
             skip_first_line = True
             for line in llm_file:
                 if not skip_first_line:
                     elements: list[str] = re.split(r'''((?:[^,"']|"[^"]*"|'[^']*')+)''', line)[1::2]
-                    labels = elements[0].split(" | ").sort()
-                    values = elements[1].removesuffix("\n").replace("\"", "").split(" | ").remove("None")
+                    labels = sorted(elements[0].replace("\"", "").split(" | "))
+                    values = (elements[1].removesuffix("\n").replace("\"", "").split(" | "))
+                    if "None" in values: values.remove("None")
                     llm_values.append((labels, values))
                 else: skip_first_line = False
 
             # Collect the labels and values from the humans
-            human_values = [()]
+            human_values = []
             human_file = open(f"../topics/{domain}/verified/{file_name}.csv", mode='r', encoding='utf-8')
             for line in human_file:
                 elements: list[str] = re.split(r'''((?:[^,"']|"[^"]*"|'[^']*')+)''', line)[1::2]
-                labels = list(set(elements[0].split(" | "))).sort()
+                labels = sorted(list(set(elements[0].replace("\"", "").split(" | "))))
                 values = elements[1].replace("\"", "").split(" | ")
                 all_values = elements[5].removesuffix("\n").replace("\"", "").split(" | ")
                 human_values.append((labels, values, all_values))
 
             # Collect the labels that were matched before the correction
-            matched_values = [()]
+            matched_values = []
             matched_file = open(f"../topics/{domain}/entities/{file_name}.csv", mode='r', encoding='utf-8')
             for line in matched_file:
                 elements: list[str] = re.split(r'''((?:[^,"']|"[^"]*"|'[^']*')+)''', line)[1::2]
-                labels = list(set(elements[0].split(" | ") + elements[4].removesuffix("\n").split(" | "))).sort()
+                labels = sorted(list(set(elements[0].replace("\"", "").split(" | ") + elements[4].removesuffix("\n").replace("\"", "").split(" | "))))
                 values = elements[2].replace("\"", "").split(" | ")
                 matched_values.append((labels, values))
 
-            output_file = open(f"../topics/{domain}/scoring/{file_name}.csv", mode='w+', encoding='utf-8')
+            output_file = open(f"../topics/{domain}/scoring_chatgpt/{file_name}.csv", mode='w+', encoding='utf-8')
 
             # Has this form: [[[label, ...], [human_value, ...], [llm_value, ...], [matched_value, ...]], [all_value, ...]], ...]
             merged_values = [[label,
                               human_value,
-                              {label: llm_value for label, llm_value in llm_values}[label],
-                              {label: matched_value for label, matched_value in matched_values}[label],
+                              {label[0]: llm_value for label, llm_value in llm_values}[label[0]],
+                              {label[0]: matched_value for label, matched_value in matched_values}[label[0]],
                               all_values]
                              for label, human_value, all_values in human_values]
 
@@ -75,14 +79,13 @@ def scoring():
                 line[4] = list(set(line[4]))
 
                 # TN
+                tn = line[4].copy()
                 for all_value in line[4]:
-                    if all_value in line[1]: line[4].remove(all_value)
-                    elif all_value in line[2]: line[4].remove(all_value)
-                tn = line[4]
+                    if all_value in line[1] or all_value in line[2]: tn.remove(all_value)
 
                 # TP
                 tp = []
-                for human_value in line[1]:
+                for human_value in line[1].copy():
                     if human_value in line[2]:
                         tp.append(human_value)
                         line[1].remove(human_value)
@@ -102,5 +105,5 @@ def scoring():
                 tn_string = f"\"{" | ".join(tn)}\""
                 fp_string = f"\"{" | ".join(fp)}\""
                 fn_string = f"\"{" | ".join(fn)}\""
-                output_columns = [len(tp), len(tn), len(fp), len(fn), tp_string, tn_string, fp_string, fn_string]
+                output_columns = [str(len(tp)), str(len(tn)), str(len(fp)), str(len(fn)), tp_string, tn_string, fp_string, fn_string]
                 output_file.write(",".join(output_columns) + "\n")
